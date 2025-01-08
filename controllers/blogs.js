43a414bc41baa437cee6478ee72605ab981a93c2
Dmytro Lobanov <dspwww@gmail.com>
2025-01-08 23:06:38 +0000

    4.17 task DONE

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,23 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body);
+blogsRouter.post('/', async (request, response) => {
+  const users = await User.find({});
+  const user = await User.findById(users[0].id);
+  // create a new blog
+  const blog = new Blog(Object.assign({}, request.body, { user: users[0].id }));
+  const result = await blog.save();
+  // Update user with new blog
+  user.blogs = user.blogs.concat(result.id);
+  await user.save();
 
-  try {
-    const result = await blog.save();
-    response.status(201).json(result);
-  } catch(error) {
-    next(error);
-  }
+  response.status(201).json(result);
 });
 
 module.exports = blogsRouter;
