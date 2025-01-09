43f6a78f333ec1a713624c22557c5803a3f09746
Dmytro Lobanov <dspwww@gmail.com>
2025-01-09 00:31:41 +0000

    4.18 task DONE

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 require('@dotenvx/dotenvx').config();
 
 const PORT = process.env.PORT;
+const SECRET = process.env.SECRET;
 const MONGODB_URI = process.env.MONGODB_URI;
 const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
   MONGODB_URI,
   NODE_ENV,
+  SECRET,
   PORT,
 };
