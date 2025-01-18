b16a7ad06b99bffe83fefc1238e1b995a35bc8cf
Dmytro Lobanov <dspwww@gmail.com>
2025-01-18 23:29:01 +0000

    Updates after part5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -25,7 +25,42 @@ blogsRouter.post('/', async (request, response) => {
   user.blogs = user.blogs.concat(result.id);
   await user.save();
 
-  response.status(201).json(result);
+  const updatedBlog = await Blog.findById(result.id);
+  await updatedBlog.populate('user');
+
+  response.status(201).json(updatedBlog);
+});
+
+blogsRouter.put('/:id', async(request, response) => {
+  const { id } = request.params;
+
+  const decodedToken = jwt.verify(request.token, SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+
+  const blog = await Blog.findOneAndUpdate({_id: id}, request.body, { new: true }).populate('user');
+  response.status(200).json(blog);
+});
+
+blogsRouter.delete('/:id', async(request, response) => {
+  const { id } = request.params;
+
+  const decodedToken = jwt.verify(request.token, SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+
+  const blog = await Blog.findOne({_id: id});
+
+  if(!blog.user.equals(decodedToken.id)) {
+    return response.status(403).json({ error: 'access denied' });
+  }
+
+  await blog.deleteOne();
+  response.status(200).end();
 });
 
 module.exports = blogsRouter;
